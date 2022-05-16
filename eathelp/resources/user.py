2f5c0036aa14bc49e2e818dd3e32a3f794ac5c05
Chubo Zeko <chubozeko@gmail.com>
2022-05-16 12:57:39 +0300

    PWP-10 List the implemented API Resources;
    - Edited Wiki
    - Added user.py to resources

---
+++ eathelp/resources/user.py
@@ -0,0 +1,16 @@
+from flask import Flask, Response, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Resource, Api
+import json
+from jsonschema import validate, ValidationError, draft7_format_checker
+from pymysql import IntegrityError
+from werkzeug.exceptions import UnsupportedMediaType, BadRequest, Conflict
+
+from eathelp import db
+from eathelp.models import User
+
+JSON = "application/json"
+
+# TODO: UserItem [PWP-50]
+
+# TODO: UserCollection [PWP-50]
