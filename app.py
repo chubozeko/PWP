55a1d1e7505cf1f6cc8afd549b5eccd8af844234
Chubo Zeko <chubozeko@gmail.com>
2022-05-22 15:52:32 +0300

    Edited default routes

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 # starts the EatHelp API app
 from eathelp import create_app
 app = create_app()
-app.run(host="0.0.0.0", port=5000)
 # app.run(debug=True)
 
 @app.route("/")
 def home_documentation():
-    return "<h1>EatHelp API</h1> " \
-           "<p>*Documentation and Usage of the API will be included here.</p>"
+    return "<h1>EatHelp API</h1>" \
+           "<p>Documentation and Usage of the API will be included here.</p>", 200
+
+@app.route("/favicon.ico")
+def favicon():
+    return "<h1>EatHelp API: favicon.ico</h1> ", 200
+
+app.run(host="0.0.0.0", port=5000)
