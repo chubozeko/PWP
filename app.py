ee9f66769c495bba57615f34f3cfbe43e07f3cf6
Chubo Zeko <chubozeko@gmail.com>
2022-05-22 15:04:34 +0300

    Added temporary home page; edited Procfile

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 # starts the EatHelp API app
 from eathelp import create_app
 app = create_app()
-app.run(debug=False)
-
+app.run()
 # app.run(debug=True)
+
+@app.route("/")
+def home_documentation():
+    return "<h1>EatHelp API</h1> " \
+           "<p>*Documentation and Usage of the API will be included here.</p>"
