3b8927d6a60eed08291e2093f74e5279ef7bae2a
Chubo Zeko <chubozeko@gmail.com>
2022-05-30 01:21:08 +0300

    PWP-54 Updated URI converters
    - Updated URI converters for resources urls with <chef>

--- eathelp/api.py
+++ eathelp/api.py
@@ -11,9 +11,9 @@ blueprint = Blueprint("api", __name__, url_prefix="/api")
 api = Api(blueprint)
 
 api.add_resource(UserCollection, "/chefs/")
-api.add_resource(UserItem, "/chefs/<chef>")     # "/chefs/<int:chef>"
-api.add_resource(CookbookCollection, "/chefs/<chef>/cookbooks/", "/cookbooks/")     # "/chefs/<int:chef>"
-api.add_resource(CookbookItem, "/chefs/<chef>/cookbooks/<int:cookbook>")    # "/chefs/<int:chef>"
+api.add_resource(UserItem, "/chefs/<int:chef>")     # "/chefs/<string:chef>"
+api.add_resource(CookbookCollection, "/chefs/<int:chef>/cookbooks/", "/cookbooks/")     # "/chefs/<string:chef>"
+api.add_resource(CookbookItem, "/chefs/<int:chef>/cookbooks/<int:cookbook>")    # "/chefs/<string:chef>"
 api.add_resource(RecipeCollection, "/chefs/<int:chef>/recipes/", "/recipes/")
 api.add_resource(RecipeItem, "/chefs/<int:chef>/recipes/<int:recipe>", "/recipes/<int:recipe>")
 api.add_resource(RecipeIngredientCollection, "/recipes/<int:recipe>/ingredients/")
@@ -22,7 +22,7 @@ api.add_resource(IngredientCollection, "/ingredients/")
 api.add_resource(IngredientItem, "/ingredients/<int:ingredient>")
 
 # Parameters used:
-# - <int:chef> = User.user_id OR <chef> = User.username
+# - <int:chef> = User.user_id OR <string:chef> = User.username
 # - <int:cookbook> = Cookbook.cookbook_id
 # - <int:recipe> = Recipe.recipe_id
 # - <int:ingredient> [from URI "/ingredients/<int:ingredient>" ] = Ingredient.ingredient_id
