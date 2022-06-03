d86191f7c6bdbca3fe47835c0cf7ca659af9ceb5
Chubo Zeko <chubozeko@gmail.com>
2022-06-03 19:27:38 +0300

    **NOTHING TO SEE HERE! (security risk)***

--- credentials.py
+++ credentials.py
@@ -1,7 +1,22 @@
 ##
-# This is the function that gets the database credentials. The database used for this API is 'pwp_db'.
+#   This code was developed by the EatHelp API team,
+#       with some code references from the following resources:
 #
-# The credential data can be filled in according to how the MySQL database was set up.
+# - Code from PWP Lectures and Exercises:
+#   (https://lovelace.oulu.fi/ohjelmoitava-web/programmable-web-project-spring-2022/)
+# - Python REST API Tutorial - Building a Flask REST API
+#   (https://www.youtube.com/watch?v=GMppyAPbLYk)
+# - Build Modern APIs using Flask (playlist)
+#   (https://www.youtube.com/playlist?list=PLMOobVGrchXN5tKYdyx-d2OwwgxJuqDVH)
+#
+#   MIT License. 2022 (c) All Rights Reserved.
+##
+
+##
+# This is the function that gets the database credentials.
+# The database used for this API is 'pwp_db'.
+#
+# The credential data should be entered according to the MySQL database setup.
 # Important credentials:
 #   - user (default: root)
 #   - password
@@ -9,12 +24,13 @@
 #   - port (MySQL default: 3306)
 # ##
 
+
 def get_db_credentials():
     return {
-        "user": "",
-        "password": "",
-        "host": "",
-        "port": "",
-        "database_name": "pwp_db",
-        "query_params": ""
+        "user": "b051e6fbeb1906",
+        "password": "0d656e0b",
+        "host": "eu-cdbr-west-02.cleardb.net",
+        "port": "3306",
+        "database_name": "heroku_5fc161546536f99",
+        "query_params": "?reconnect=true"
     }
